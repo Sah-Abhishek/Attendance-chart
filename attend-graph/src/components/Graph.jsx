e439d9712aa74f32b4d851e05923cddcc07e61fb
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-09-20 21:35:21 +0530

    commit

--- attend-graph/src/components/Graph.jsx
+++ attend-graph/src/components/Graph.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 
+
 const AttendanceChart = ({ dates, percentages }) => {
     const [lineChartData, setLineChartData] = useState({
         labels: [],
